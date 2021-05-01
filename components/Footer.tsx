25ddab4bf1d431bc44c4cbbe371001d6ec7163d8
wilkersoh <plgu@hotmail.com>
2021-05-02 02:21:53 +0800

    we stayed private

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div className='green'>
-      <div>i am footers</div>
-    </div>
-  );
-}
--- pages/404.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CannotFound404() {
-  return <div>CannotFound404</div>;
-}
