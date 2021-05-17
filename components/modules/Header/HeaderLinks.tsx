8dd9fc64e2f5acbae85fa297f274c9832d1e4cef
wilkersoh <plgu@hotmail.com>
2021-05-17 15:28:38 +0800

    update: edit mobile transition speed

--- components/modules/Header/HeaderLinks.tsx
+++ components/modules/Header/HeaderLinks.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import Link from "next/link";
 import { LinkPathsType } from "@/components/modules/Header/interfaces";
 
-const LinkPaths: LinkPathsType[] = [
-  { route: "/", name: "home" },
-  { route: "/blog", name: "blog" },
-  { route: "/about", name: "about" },
+export const LinkPaths: LinkPathsType[] = [
+  { route: "/", name: "首頁" },
+  { route: "/blog", name: "文章" },
+  { route: "/about", name: "關於我" },
 ];
 
-const HeaderLinks: React.FC<LinkPathsType> = ({ route, name }) => {
+const HeaderLinks: React.FC = () => {
   return (
-    <div>
-      <div>Links paths</div>
-    </div>
+    <nav className='hidden md:block ml-auto mr-10'>
+      <ul className="flex space-x-4">
+        {LinkPaths.map(({ route, name }) => (
+          <li key={route}>
+            <Link href={route}>
+              <a>{name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
